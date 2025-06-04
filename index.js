f5e0fcbf177620db74a8440f7be1c8af66580b5a
Vivian Gichure <viviangichure@gmail.com>
2025-06-04 22:05:19 +0300

    completed lab

--- index.js
+++ index.js
@@ -23,3 +23,25 @@ module.exports = {
   returnsANamedFunction,
   returnsAnAnonymousFunction,
 };
+
+
+/*receivesAFunction(callback)*/
+function receivesAFunction(callback) {
+  callback();
+}
+
+/*returnsANamedFunction()*/
+function returnsANamedFunction() {
+  function namedFunction() {
+    console.log("I am a named function");
+  }
+  return namedFunction;
+}
+
+/*returnsAnAnonymousFunction()*/
+function returnsAnAnonymousFunction() {
+  return function () {
+    console.log("I am an anonymous function");
+  };
+}
+
