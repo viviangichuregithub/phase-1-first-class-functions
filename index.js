8bf01e1060ccf3911f7aa33519eb263aff97ed52
Vivian Gichure <viviangichure@gmail.com>
2025-06-04 16:45:22 +0300

    completed lab

---
+++ index.js
@@ -0,0 +1,25 @@
+// 1. Takes a callback function and calls it
+function receivesAFunction(callback) {
+  callback();
+}
+
+// 2. Returns a named function
+function returnsANamedFunction() {
+  return function namedFunction() {
+    console.log("I'm a named function");
+  };
+}
+
+// 3. Returns an anonymous function
+function returnsAnAnonymousFunction() {
+  return function () {
+    console.log("I'm an anonymous function");
+  };
+}
+
+// Export the functions for testing
+module.exports = {
+  receivesAFunction,
+  returnsANamedFunction,
+  returnsAnAnonymousFunction,
+};
